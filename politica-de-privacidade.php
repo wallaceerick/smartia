18de637934753aacf941c02ef506d699d47d9980
Wallace Erick <wallace_erick@hotmail.com>
2013-09-24 22:01:35 -0300

    Política de Privacidade

---
+++ politica-de-privacidade.php
@@ -0,0 +1,108 @@
+<!DOCTYPE HTML>
+	<html lang="pt-BR">
+		
+	<head>
+		<meta charset="UTF-8">
+		<title>Política de Privacidade</title>
+		
+		<meta name="author" content="Smartia" />
+		<meta name="keywords" content="" />
+		<meta name="description" content="" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+
+		<link rel="shortcut icon" href="static/v3/images/favico.ico">
+		<link rel="apple-touch-icon-precomposed" href="static/v3/images/layout/icon-ios.png">
+		
+		<link rel="stylesheet" href="static/v3/styles/reset.css" media="all" />  
+		<link rel="stylesheet" href="static/v3/styles/bootstrap.css" media="all" />  
+		<link rel="stylesheet" href="static/v3/styles/inside.css" media="all" /> 
+		<link rel="stylesheet" href="static/v3/styles/common.css" media="all" /> 
+		<link rel="stylesheet" href="static/v3/styles/header.css" media="all" /> 
+		<link rel="stylesheet" href="static/v3/styles/footer.css" media="all" />
+		<link rel="stylesheet" href="static/v3/styles/inputs.css" media="all" />
+		<link rel="stylesheet" href="static/v3/styles/select.css" media="all" />
+		
+		<!--[if IE 8]>
+			<script type="text/javascript" src="static/v3/scripts/ie8.min.js"></script>
+		<![endif]-->
+		
+		<!-- Typekit -->
+		<script type="text/javascript" src="//use.typekit.net/sxe8qvx.js"></script>
+		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+		<script>
+			var WAIT_FONTS_INTERVAL = 10;
+			var WAIT_FONTS_TIMEOUT = 10000;
+			var WAIT_TIME = 0;  
+			function waitFonts() {
+				WAIT_TIME += WAIT_FONTS_INTERVAL;
+				var html = document.getElementsByTagName('html')[0];
+				if (html.className.indexOf('wf-active') > -1 || WAIT_TIME > WAIT_FONTS_TIMEOUT) {
+					html.className = html.className.replace(/\bwaitfonts\b/,'');
+				} else {
+					window.setTimeout(waitFonts, WAIT_FONTS_INTERVAL);
+				}
+			}
+			waitFonts();
+		</script>
+		
+	</head>
+	
+	<body>
+		
+		<?php include('includes/header.php'); ?>
+		
+		<div class="page-title">
+			<div class="container single-page">		
+			<ol class="breadcrumb">
+			  <li><a href="#">Home</a></li> 
+			  <li class="active">Política de Privacidade</li>
+			</ol>
+			<h1>Política de Privacidade</h1>
+			<a href="#" class="big-simule-button">Simule agora seu seguro</a>
+			</div>
+		</div>
+		
+		<div class="container">	
+			<span class="double-border"></span>
+			
+			<div class="row">
+				<div class="col-md-12 col-sm-12 col-xs-12">
+					<section class="page-with-aside with-padding institucional"> 
+						
+						<h3 class="yellow mb-15">Política de privacidade de corretora de seguro online</h3>
+						<p>A privacidade dos clientes da Smartia é extremamente importante. A seguir descreveremos o tipo de informação que coletamos e como utilizamos e armazenamos esta informação. Lembre-se: nunca venderemos a sua informação para terceiros, sejam eles particulares ou empresas.</p>
+						
+						<h4 class="yellow mb-15">Coleta de informações</h4>
+						<p>Tal como outros sites, a Smartia utiliza basicamente suas informações de navegação, como por exemplo o seu endereço IP (Internet Protocol), o seu ISP (Internet Service Provider), o browser que utilizou ao visitar a Smartia (como o Internet Explorer ou o Firefox), o tempo da sua visita e quais páginas visitou.</p>
+						<p>Para a realização da cotação online a Smartia obtém as informações pessoais necessárias de você, cliente, de forma voluntária. Você é convidado a preencher um formulário onde informa alguns de seus dados pessoais como nome, endereço, informações do veículo, CPF, etc. Nós desejamos que você sinta-se confortável e seguro de que todas as informações fornecidas estão sendo protegidas com rigoroso cuidado pela Smartia, preservando sua privacidade.</p>
+						<p>A Smartia também pode utilizar cookies e/ou web beacons quando você tem acesso às suas páginas. Os cookies associam-se unicamente com o navegador de um determinado computador.</p>
+						<p>Em adição também poderemos eventualmente utilizar publicidade de terceiros na Smartia. Alguns destes publicitários poderão utilizar as mesmas tecnologias acima descritas em seus anúncios, o que fará com que esses publicitários (como o Google, através do Google AdSense) também recebam suas informações de navegação. Isso geralmente é utilizado para Geotargeting (mostrar publicidade de São Paulo apenas aos clientes de São Paulo, por exemplo) ou apresentar publicidade direcionada a um determinado tipo de utilizador (como mostrar publicidade de restaurante a um utilizador que visita sites de culinária regularmente, por exemplo).</p>
+						<p>Você poderá pesquisar o disco rígido de seu computador, conforme instruções do seu próprio navegador.</p>
+						<p>Você também tem a possibilidade de configurar seu navegador para ser avisado, na tela do computador, sobre a recepção dos cookies e para impedir a sua instalação no disco rígido. As informações pertinentes a esta configuração estão disponíveis em instruções e manuais do próprio navegador.</p>
+						
+						<h4 class="yellow mb-15">Uso das informações</h4>
+						<p>Seus dados serão utilizados para nos ajudar a prover o melhor serviço possível, tentando sempre adequá-lo às suas necessidades e características pessoais.</p>
+						<p>Também deve ficar claro que seus dados pessoais informados serão sempre transmitidos para as seguradoras parceiras, no momento da realização dos cálculos e posteriormente na emissão das propostas de seguro. Novamente, nenhuma informação pessoal é repassada para qualquer outra entidade que não as seguradoras parceiras.</p>
+						<p>As informações de navegação, conforme descritas acima, são disponibilizadas para a ferramenta "Google Analytics" para que a consolidação das mesmas nos forneça um mapa da utilização da Smartia e do que possivelmente possa ser evoluído no serviço.</p>
+						<p>Todas as informações manipuladas pelo nosso serviço são protegidas e armazenadas em nossos servidores com sigilo e segurança.</p>
+						
+					</section>
+					
+					<aside class="aside-with-page">
+						<!-- Sidebar content -->
+					</aside>
+					
+					<br />
+					
+				</div>
+			</div>
+		</div>
+		
+		<?php include('includes/footer.php'); ?>
+		<?php include('includes/modal.php'); ?>
+		
+		<?php include('includes/scripts.php'); ?>
+		
+	</body>
+</html>
