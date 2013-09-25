109bf61546b192cebc1d0dc52318bf381508fca2
Wallace Erick <wallace_erick@hotmail.com>
2013-09-24 23:35:52 -0300

    Depoimentos

---
+++ depoimentos.php
@@ -0,0 +1,210 @@
+<!DOCTYPE HTML>
+	<html lang="pt-BR">
+		
+	<head>
+		<meta charset="UTF-8">
+		<title>Depoimentos</title>
+		
+		<meta name="author" content="Smartia" />
+		<meta name="keywords" content="" />
+		<meta name="description" content="" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+
+		<link rel="shortcut icon" href="static/v3/images/favico.ico">
+		<link rel="apple-touch-icon-precomposed" href="static/v3/images/layout/icon-ios.png">
+		
+		<link rel="stylesheet" href="static/v3/styles/reset.css" media="all" />  
+		<link rel="stylesheet" href="static/v3/styles/bootstrap.css" media="all" />  
+		<link rel="stylesheet" href="static/v3/styles/inside.css" media="all" /> 
+		<link rel="stylesheet" href="static/v3/styles/common.css" media="all" /> 
+		<link rel="stylesheet" href="static/v3/styles/header.css" media="all" /> 
+		<link rel="stylesheet" href="static/v3/styles/footer.css" media="all" />
+		<link rel="stylesheet" href="static/v3/styles/inputs.css" media="all" />
+		<link rel="stylesheet" href="static/v3/styles/select.css" media="all" />
+		
+		<!--[if IE 8]>
+			<script type="text/javascript" src="static/v3/scripts/ie8.min.js"></script>
+		<![endif]-->
+		
+		<!-- Typekit -->
+		<script type="text/javascript" src="//use.typekit.net/sxe8qvx.js"></script>
+		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+		<script>
+			var WAIT_FONTS_INTERVAL = 10;
+			var WAIT_FONTS_TIMEOUT = 10000;
+			var WAIT_TIME = 0;  
+			function waitFonts() {
+				WAIT_TIME += WAIT_FONTS_INTERVAL;
+				var html = document.getElementsByTagName('html')[0];
+				if (html.className.indexOf('wf-active') > -1 || WAIT_TIME > WAIT_FONTS_TIMEOUT) {
+					html.className = html.className.replace(/\bwaitfonts\b/,'');
+				} else {
+					window.setTimeout(waitFonts, WAIT_FONTS_INTERVAL);
+				}
+			}
+			waitFonts();
+		</script>
+		
+	</head>
+	
+	<body>
+		
+		<?php include('includes/header.php'); ?>
+		
+		<div class="page-title">
+			<div class="container single-page">		
+			<ol class="breadcrumb">
+			  <li><a href="#">Home</a></li> 
+			  <li><a href="#">Sobre a Smartia</a></li> 
+			  <li class="active">Depoimentos</li>
+			</ol>
+			<h1>Depoimentos</h1>
+			<a href="#" class="big-simule-button">Simule agora seu seguro</a>
+			</div>
+		</div>
+		
+		<div class="container">	
+			<span class="double-border"></span>
+			
+			<div class="row">
+				<div class="col-md-12 col-sm-12 col-xs-12">
+					<section class="page-with-aside with-padding institucional"> 
+						
+						<h3 class="yellow mb-15">Quem contratou, curtiu!</h3>
+						<div class="depoimentos">
+							<figure class="item active depoimento">
+								<blockquote>
+									<span>Apesar de ter desistido da compra do meu carro e consequentemente do seguro, agradeço muito pelo atendimento que a Smartia me proporcionou.</span>
+									<b class="seta"></b>
+								</blockquote>
+								<figcaption class="clearfix">
+									<img src="static/v3/images/home/depoimentos/sem-foto.png" alt="Edson Aversa Rinbon">
+										<span class="autor">Edson Aversao Rinbon</span>
+										<span class="localidade">Itajaí - RS</span>
+								</figcaption>
+							</figure>
+						</div> 
+						
+						<div class="depoimentos">
+							<figure class="item active depoimento">
+								<blockquote>
+									<span>Apesar de ter desistido da compra do meu carro e consequentemente do seguro, agradeço muito pelo atendimento que a Smartia me proporcionou.</span>
+									<b class="seta"></b>
+								</blockquote>
+								<figcaption class="clearfix">
+									<img src="static/v3/images/home/depoimentos/sem-foto.png" alt="Edson Aversa Rinbon">
+										<span class="autor">Edson Aversao Rinbon</span>
+										<span class="localidade">Itajaí - RS</span>
+								</figcaption>
+							</figure>
+						</div> 
+						
+						<div class="depoimentos">
+							<figure class="item active depoimento">
+								<blockquote>
+									<span>Apesar de ter desistido da compra do meu carro e consequentemente do seguro, agradeço muito pelo atendimento que a Smartia me proporcionou.</span>
+									<b class="seta"></b>
+								</blockquote>
+								<figcaption class="clearfix">
+									<img src="static/v3/images/home/depoimentos/sem-foto.png" alt="Edson Aversa Rinbon">
+										<span class="autor">Edson Aversao Rinbon</span>
+										<span class="localidade">Itajaí - RS</span>
+								</figcaption>
+							</figure>
+						</div> 
+						
+						<div class="depoimentos">
+							<figure class="item active depoimento">
+								<blockquote>
+									<span>Apesar de ter desistido da compra do meu carro e consequentemente do seguro, agradeço muito pelo atendimento que a Smartia me proporcionou.</span>
+									<b class="seta"></b>
+								</blockquote>
+								<figcaption class="clearfix">
+									<img src="static/v3/images/home/depoimentos/sem-foto.png" alt="Edson Aversa Rinbon">
+										<span class="autor">Edson Aversao Rinbon</span>
+										<span class="localidade">Itajaí - RS</span>
+								</figcaption>
+							</figure>
+						</div> 
+						
+						<div class="depoimentos">
+							<figure class="item active depoimento">
+								<blockquote>
+									<span>Apesar de ter desistido da compra do meu carro e consequentemente do seguro, agradeço muito pelo atendimento que a Smartia me proporcionou.</span>
+									<b class="seta"></b>
+								</blockquote>
+								<figcaption class="clearfix">
+									<img src="static/v3/images/home/depoimentos/sem-foto.png" alt="Edson Aversa Rinbon">
+										<span class="autor">Edson Aversao Rinbon</span>
+										<span class="localidade">Itajaí - RS</span>
+								</figcaption>
+							</figure>
+						</div> 
+						
+						<div class="depoimentos">
+							<figure class="item active depoimento">
+								<blockquote>
+									<span>Apesar de ter desistido da compra do meu carro e consequentemente do seguro, agradeço muito pelo atendimento que a Smartia me proporcionou.</span>
+									<b class="seta"></b>
+								</blockquote>
+								<figcaption class="clearfix">
+									<img src="static/v3/images/home/depoimentos/sem-foto.png" alt="Edson Aversa Rinbon">
+										<span class="autor">Edson Aversao Rinbon</span>
+										<span class="localidade">Itajaí - RS</span>
+								</figcaption>
+							</figure>
+						</div> 
+						
+						<div class="depoimentos">
+							<figure class="item active depoimento">
+								<blockquote>
+									<span>Apesar de ter desistido da compra do meu carro e consequentemente do seguro, agradeço muito pelo atendimento que a Smartia me proporcionou.</span>
+									<b class="seta"></b>
+								</blockquote>
+								<figcaption class="clearfix">
+									<img src="static/v3/images/home/depoimentos/sem-foto.png" alt="Edson Aversa Rinbon">
+										<span class="autor">Edson Aversao Rinbon</span>
+										<span class="localidade">Itajaí - RS</span>
+								</figcaption>
+							</figure>
+						</div>
+						
+						<br />
+						
+						<div class="pagination">
+							<ul>
+								<li><a class="first" href="#">»</a></li>
+								<li><a href="#">1</a></li>
+								<li><a href="#">2</a></li>
+								<li><a href="#">3</a></li>
+								<li><a href="#">4</a></li>
+								<li><a href="#">5</a></li>
+								<li><a class="last" href="#">»</a></li>
+							</ul>
+						</div>
+						
+					</section>
+					
+					<aside class="aside-with-page">
+						<ul class="aside-links">
+							<li><a class="active" href="seguro-online.php">Seguro Online</a></li>
+							<li><a href="acionando-seu-seguro.php">Acionando seu seguro</a></li>
+							<li><a href="#">FAQ - Perguntas Frequentes</a></li>
+							<li><a class="last" href="dicionario-de-segures.php">Dicionário do “Segurês”</a></li>
+						</ul>
+					</aside>
+					
+					<br />
+					
+				</div>
+			</div>
+		</div>
+		
+		<?php include('includes/footer.php'); ?>
+		<?php include('includes/modal.php'); ?>
+		
+		<?php include('includes/scripts.php'); ?>
+		
+	</body>
+</html>
