395b60e1a6584429c2f549b3316415cde45103d4
Wallace Erick <wallace_erick@hotmail.com>
2013-09-25 00:00:05 -0300

    FAQ

---
+++ faq.php
@@ -0,0 +1,259 @@
+<!DOCTYPE HTML>
+	<html lang="pt-BR">
+		
+	<head>
+		<meta charset="UTF-8">
+		<title>FAQ - Perguntas Frequentes</title>
+		
+		<meta name="author" content="Smartia" />
+		<meta name="keywords" content="" />
+		<meta name="description" content="" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+
+		<link rel="shortcut icon" href="static/v3/images/favico.ico">
+		<link rel="apple-touch-icon-precomposed" href="static/v3/images/layout/icon-ios.png">
+		
+		<link rel="stylesheet" href="static/v3/styles/reset.css" media="all" />  
+		<link rel="stylesheet" href="static/v3/styles/bootstrap.css" media="all" />  
+		<link rel="stylesheet" href="static/v3/styles/inside.css" media="all" /> 
+		<link rel="stylesheet" href="static/v3/styles/common.css" media="all" /> 
+		<link rel="stylesheet" href="static/v3/styles/header.css" media="all" /> 
+		<link rel="stylesheet" href="static/v3/styles/footer.css" media="all" />
+		<link rel="stylesheet" href="static/v3/styles/inputs.css" media="all" />
+		<link rel="stylesheet" href="static/v3/styles/select.css" media="all" />
+		
+		<!--[if IE 8]>
+			<script type="text/javascript" src="static/v3/scripts/ie8.min.js"></script>
+		<![endif]-->
+		
+		<!-- Typekit -->
+		<script type="text/javascript" src="//use.typekit.net/sxe8qvx.js"></script>
+		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+		<script>
+			var WAIT_FONTS_INTERVAL = 10;
+			var WAIT_FONTS_TIMEOUT = 10000;
+			var WAIT_TIME = 0;  
+			function waitFonts() {
+				WAIT_TIME += WAIT_FONTS_INTERVAL;
+				var html = document.getElementsByTagName('html')[0];
+				if (html.className.indexOf('wf-active') > -1 || WAIT_TIME > WAIT_FONTS_TIMEOUT) {
+					html.className = html.className.replace(/\bwaitfonts\b/,'');
+				} else {
+					window.setTimeout(waitFonts, WAIT_FONTS_INTERVAL);
+				}
+			}
+			waitFonts();
+		</script>
+		
+	</head>
+	
+	<body>
+		
+		<?php include('includes/header.php'); ?>
+		
+		<div class="page-title">
+			<div class="container single-page">		
+			<ol class="breadcrumb">
+			  <li><a href="#">Home</a></li> 
+			  <li><a href="#">Como Funciona</a></li> 
+			  <li class="active">FAQ - Perguntas Frequentes</li>
+			</ol>
+			<h1>FAQ - Perguntas Frequentes</h1>
+			<a href="#" class="big-simule-button">Simule agora seu seguro</a>
+			</div>
+		</div>
+		
+		<div class="container">	
+			<span class="double-border"></span>
+			
+			<div class="row">
+				<div class="col-md-12 col-sm-12 col-xs-12">
+					<section class="page-with-aside with-padding institucional"> 
+						
+						<h3 class="yellow mb-15">Dúvidas sobre seguro online</h3>
+						
+						<div class="faq">
+							<h4 class="accordion-title">Como é o processo de contratação e cobertura do risco no seguro de automóvel?</h4>
+							<div class="accordion-content current">
+								<p>A contratação se dá através de:</p>
+								<p>
+									1) Entrega da proposta de seguro, com respectivo protocolo (físico ou eletrônico); <br /> 
+									2) Inspeção do risco, nota fiscal ou confirmação de renovação;  <br />
+									3) Pagamento da primeira parcela ou parcela única dentro do vencimento estipulado pela seguradora. 
+								</p>
+								<p>É comum a concessão do prazo de 3 a 5 dias úteis (ou corridos) para pagamento da primeira parcela. Após a realização dos três passos, a seguradora dispõe de 15 dias para analisar e, eventualmente, recusar o risco. Considerando que o risco esteja dentro da política de aceitação da companhia divulgada aos corretores, até que a eventual recusa seja consumada ao corretor o risco está coberto, exceto nos casos de fraude comprovada.</p>
+							</div>
+						</div>
+						
+						<hr class="dotted full line" />
+						
+						<div class="faq">
+							<h4 class="accordion-title">Quais as principais garantias oferecidas no seguro de automóvel?</h4>
+							<div class="accordion-content">
+								<p>CONTENT!</p>
+							</div>
+						</div>
+						
+						<hr class="dotted full line" />
+						
+						<div class="faq">
+							<h4 class="accordion-title">O que são coberturas básicas?</h4>
+							<div class="accordion-content">
+								<p>CONTENT!</p>
+							</div>
+						</div>
+						
+						<hr class="dotted full line" />
+						
+						<div class="faq">
+							<h4 class="accordion-title">Quais são as modalidades de contratação do seguro de automóvel?</h4>
+							<div class="accordion-content">
+								<p>CONTENT!</p>
+							</div>
+						</div>
+						
+						<hr class="dotted full line" />
+						
+						<div class="faq">
+							<h4 class="accordion-title">O que são coberturas adicionais?</h4>
+							<div class="accordion-content">
+								<p>CONTENT!</p>
+							</div>
+						</div>
+						
+						<hr class="dotted full line" />
+						
+						<div class="faq">
+							<h4 class="accordion-title">O que cobre a garantia da blindagem do veículo?</h4>
+							<div class="accordion-content">
+								<p>CONTENT!</p>
+							</div>
+						</div>
+						
+						<hr class="dotted full line" />
+						
+						<div class="faq">
+							<h4 class="accordion-title">O que cobre a garantia do kit gás?</h4>
+							<div class="accordion-content">
+								<p>CONTENT!</p>
+							</div>
+						</div>
+						
+						<hr class="dotted full line" />
+						
+						<div class="faq">
+							<h4 class="accordion-title">O que cobre a garantia de carro reserva?</h4>
+							<div class="accordion-content">
+								<p>CONTENT!</p>
+							</div>
+						</div>
+						
+						<hr class="dotted full line" />
+						
+						<div class="faq">
+							<h4 class="accordion-title">O que é a garantia de despesas extraordinárias?</h4>
+							<div class="accordion-content">
+								<p>CONTENT!</p>
+							</div>
+						</div>
+						
+						<hr class="dotted full line" />
+						
+						<div class="faq">
+							<h4 class="accordion-title">O que é cobertura de RCF (Responsabilidade Civil Facultativa)?</h4>
+							<div class="accordion-content">
+								<p>CONTENT!</p>
+							</div>
+						</div>
+						
+						<hr class="dotted full line" />
+						
+						<div class="faq">
+							<h4 class="accordion-title">O que é cobertura de APP (Acidentes Pessoais de Passageiros)?</h4>
+							<div class="accordion-content">
+								<p>CONTENT!</p>
+							</div>
+						</div>
+						
+						<hr class="dotted full line" />
+						
+						<div class="faq">
+							<h4 class="accordion-title">O que cobre os Serviços de Assistência 24hs?</h4>
+							<div class="accordion-content">
+								<p>CONTENT!</p>
+							</div>
+						</div>
+						
+						<hr class="dotted full line" />
+						
+						<div class="faq">
+							<h4 class="accordion-title">O que é franquia?</h4>
+							<div class="accordion-content">
+								<p>CONTENT!</p>
+							</div>
+						</div>
+						
+						<hr class="dotted full line" />
+						
+						<div class="faq">
+							<h4 class="accordion-title">O que é bônus e como funciona o critério de bonificação?</h4>
+							<div class="accordion-content">
+								<p>CONTENT!</p>
+							</div>
+						</div>
+						
+						<hr class="dotted full line" />
+						
+						<div class="faq">
+							<h4 class="accordion-title">Como as Seguradoras avaliam o risco e precificam o valor do seguro do meu carro?</h4>
+							<div class="accordion-content">
+								<p>CONTENT!</p>
+							</div>
+						</div>
+						
+						<hr class="dotted full line" />
+						
+						<div class="faq">
+							<h4 class="accordion-title">O que devo fazer em caso de sinistro?</h4>
+							<div class="accordion-content">
+								<p>CONTENT!</p>
+							</div>
+						</div>
+						
+						<hr class="dotted full line" />
+						
+						<div class="faq">
+							<h4 class="accordion-title">A Seguradora pode recusar a fazer o seguro do meu automóvel?</h4>
+							<div class="accordion-content">
+								<p>CONTENT!</p>
+							</div>
+						</div>
+						
+						<p class="alert">É fundamental a participação do CORRETOR DE SEGUROS no assessoramento ao cliente no momento da contratação do Seguro de Automóveis.</p>
+						 
+						
+					</section>
+					
+					<aside class="aside-with-page">
+						<ul class="aside-links">
+							<li><a href="seguro-online.php">Seguro Online</a></li>
+							<li><a href="acionando-seu-seguro.php">Acionando seu seguro</a></li>
+							<li><a class="active" href="faq.php">FAQ - Perguntas Frequentes</a></li>
+							<li><a class="last" href="dicionario-de-segures.php">Dicionário do “Segurês”</a></li>
+						</ul>
+					</aside>
+					
+					<br />
+					
+				</div>
+			</div>
+		</div>
+		
+		<?php include('includes/footer.php'); ?>
+		<?php include('includes/modal.php'); ?>
+		
+		<?php include('includes/scripts.php'); ?>
+		
+	</body>
+</html>
--- static/v3/scripts/_custom.js
+++ static/v3/scripts/_custom.js
@@ -7,4 +7,10 @@ $(function(){
 	$(".tel").mask("(99) 9999-9999", {
 		//placeholder:"Ex: (XX) 9999-9999"
 	});
+
+	//Accordion
+	$('h4.accordion-title').click(function(){
+		$(this).parent().find('div.accordion-content').slideToggle(300);
+	});
+	
 });
